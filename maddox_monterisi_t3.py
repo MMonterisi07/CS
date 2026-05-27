c05c6d4ffcfb577ff05b6ef7040db2ab6ee06224
MMonterisi07 <mmonterisi28@gcds.net>
2026-05-27 12:09:33 -0400

    Add files via upload

--- maddox_monterisi_t3.py
+++ maddox_monterisi_t3.py
@@ -1,13 +1,20 @@
 '''
 Name: Maddox Monterisi
-Date: 4/2/26
-Description: creates a data dictionary of the most common words in Macbeth and Romeo and Juliet
+Date: 4/7/26
+Description: Runs a tic tac toe game
 Bugs: none
 Sources: Will Everett taught me how to do the board
-Version 1.0
+Version 1.1
 '''
 
 def display_board(board):
+    '''
+    Prints the tic-tac-toe board.
+    Parameters: 
+        board (list): 3x3 2D list of strings
+    Returns: 
+        None
+    '''
 
     print(f'''   1     2     3
 1  {board[0][0]}  |  {board[0][1]}  |  {board[0][2]}
@@ -20,7 +27,14 @@ def display_board(board):
 
 
 def get_player_move(board, player):
-
+    '''
+    Prompts the current player to enter a row and column and rejects squares that don't fit the criteria
+    Parameters: 
+        board (list) - 3x3 list
+        player (str): 'x' or 'o'
+    Returns: 
+        row_index, column_index
+    '''
     while True:
         print(f"Player {player}'s turn")
     
@@ -33,74 +47,91 @@ def get_player_move(board, player):
             column = int(input('Enter a column: '))
         except ValueError:
             print("That is not a number. ")
+            continue
         
         if row < 1 or row > 3 or column < 1 or column > 3:
             print('Number must be between 1 and 3. ')
             continue
-
+ 
         row_index = row - 1
         column_index = column - 1
-
+ 
         if board[row_index][column_index] != " ":
             print('That spot is already taken. Pick a different one.')
             continue
         return row_index, column_index
 
 def check_winner(board):
-
-    if board[0][0] == 'x' and board [0][1] == 'x' and board [0][2] == 'x':
-        return 'X'
-    elif board[1][0] == 'x' and board [1][1] == 'x' and board [1][2] == 'x':
-        return 'X'
-    elif board[2][0] == 'x' and board[2][1] == 'x' and board [2][2] == 'x':
-        return 'X'
-    
-    elif board[0][0] == 'x' and board[1][0] == 'x' and board [2][0] == 'x':
+    '''
+    Checks all rows, columns, and diagonals for a winning combination
+    Parameters: 
+        board (list): 3x3 list
+    Returns: 
+        'X' if x wins, 'O' if o wins, None if no winner yet
+    '''
+    if board[0][0] == 'x' and board[0][1] == 'x' and board[0][2] == 'x':
         return 'X'
     elif board[1][0] == 'x' and board[1][1] == 'x' and board[1][2] == 'x':
         return 'X'
-    elif board[2][0] == 'x' and board[2][1] == 'x' and board [2][2] == 'x':
+    elif board[2][0] == 'x' and board[2][1] == 'x' and board[2][2] == 'x':
         return 'X'
-    
-    elif board[0][0] == 'x' and board[1][1] == 'x' and board [2][2] == 'x':
+ 
+    elif board[0][0] == 'x' and board[1][0] == 'x' and board[2][0] == 'x':
+        return 'X'
+    elif board[0][1] == 'x' and board[1][1] == 'x' and board[2][1] == 'x':
+        return 'X'
+    elif board[0][2] == 'x' and board[1][2] == 'x' and board[2][2] == 'x':
+        return 'X'
+ 
+    elif board[0][0] == 'x' and board[1][1] == 'x' and board[2][2] == 'x':
         return 'X'
     elif board[2][0] == 'x' and board[1][1] == 'x' and board[0][2] == 'x':
         return 'X'
-    
-
-    if board[0][0] == 'o' and board [0][1] == 'o' and board [0][2] == '':
-        return 'O'
-    elif board[1][0] == 'o' and board [1][1] == 'o' and board [1][2] == 'o':
-        return 'O'
-    elif board[2][0] == 'o' and board[2][0] == 'o' and board [2][2] == 'o':
-        return 'O'
-    
-    elif board[0][0] == 'o' and board[1][0] == 'o' and board [2][0] == 'o':
+ 
+    elif board[0][0] == 'o' and board[0][1] == 'o' and board[0][2] == 'o':
         return 'O'
     elif board[1][0] == 'o' and board[1][1] == 'o' and board[1][2] == 'o':
         return 'O'
-    elif board[2][0] == 'o' and board[2][1] == 'o' and board [2][2] == 'o':
+    elif board[2][0] == 'o' and board[2][1] == 'o' and board[2][2] == 'o':
         return 'O'
-    
-    elif board[0][0] == 'o' and board[1][1] == 'o' and board [2][2] == 'o':
+ 
+    elif board[0][0] == 'o' and board[1][0] == 'o' and board[2][0] == 'o':
+        return 'O'
+    elif board[0][1] == 'o' and board[1][1] == 'o' and board[2][1] == 'o':
+        return 'O'
+    elif board[0][2] == 'o' and board[1][2] == 'o' and board[2][2] == 'o':
+        return 'O'
+ 
+    elif board[0][0] == 'o' and board[1][1] == 'o' and board[2][2] == 'o':
         return 'O'
     elif board[2][0] == 'o' and board[1][1] == 'o' and board[0][2] == 'o':
         return 'O'
  
 
 def is_draw(board):
-  
+    '''
+    Checks whether the game is a draw
+    Parameters: 
+        board (list): 3x3 2D list
+    Returns: 
+        True if the board is full, False if any empty square remains
+    '''
     for row in range(3):
-        if board[row] == ' ':
-            return True
-    for column in range(3):
-        if board[column] == ' ':
-            return True
-
+        for column in range(3):
+            if board[row][column] == ' ':
+                return False
+    return True
 
 
 def play_game():
-
+    '''
+    Sets up the board and runs the main game loop, alternating turns between
+    players until someone wins or the game ends in a draw.
+    Parameters: 
+        None
+    Returns: 
+        None
+    '''
     board = [
         [" ", " ", " "],
         [" ", " ", " "],
@@ -109,28 +140,26 @@ def play_game():
     current_player = 'x'
     print('Welcome to Tic-Tac-Toe! Player x goes first. ')
     display_board(board)
-
+ 
     while True:
         row, column = get_player_move(board, current_player)
         board[row][column] = current_player
         display_board(board)
-
+ 
         winner = check_winner(board)
-        if winner is True:
+        if winner:
             print(f'Player {winner} wins! ')
             break
-
+ 
         if is_draw(board):
             print('Its a draw! ')
             break
-
+ 
         if current_player == 'x':
             current_player = 'o'
         elif current_player == 'o':
             current_player = 'x'
-
-
-
+ 
  
 if __name__ == '__main__':
     while True:
